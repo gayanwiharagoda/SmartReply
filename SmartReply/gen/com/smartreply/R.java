10fbbd0937cb757329f76db4e06adebdce84e34a
gayan <gayan@gayan-Satellite-L655>
2013-01-15 06:10:12 +0530

    welcome,event Layout has been created

--- SmartReply/gen/com/smartreply/R.java
+++ SmartReply/gen/com/smartreply/R.java
@@ -15,10 +15,19 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int editText1=0x7f070002;
+        public static final int editText2=0x7f070003;
+        public static final int listView1=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int spinner1=0x7f070004;
+        public static final int toggleButton1=0x7f070005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int events_layout=0x7f030000;
+        public static final int group_layout=0x7f030001;
+        public static final int main_layout=0x7f030002;
+        public static final int new_event_layout=0x7f030003;
+        public static final int welcome_layout=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
---
+++ SmartReply/src/com/smartreply/Activity/MainActivity.java
@@ -0,0 +1,65 @@
+package com.smartreply.Activity;
+
+import com.smartreply.R;
+
+import android.os.Bundle;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+public class MainActivity extends TabActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+		tabArranging();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private void tabArranging(){
+		
+	       TabHost tabHost = getTabHost();
+	       
+	        // Tab for Welcome
+	        TabSpec welcomeSpec = tabHost.newTabSpec("Welcome");
+	        // setting Title and Icon for the Tab
+	        //photospec.setIndicator("Photos", getResources().getDrawable(R.drawable.icon_photos_tab));
+	        welcomeSpec.setIndicator("Home");
+	        Intent welcomeIntent = new Intent(this, WelcomeActivity.class);
+	        welcomeSpec.setContent(welcomeIntent);
+	 
+	        // Tab for Event
+	        TabSpec eventSpec = tabHost.newTabSpec("Events");
+	        // setting Title and Icon for the Tab
+	        //photospec.setIndicator("Photos", getResources().getDrawable(R.drawable.icon_photos_tab));
+	        eventSpec.setIndicator("Events");
+	        Intent eventIntent = new Intent(this, EventsActivity.class);
+	        eventSpec.setContent(eventIntent);
+	   
+	        
+	     // Tab for Event
+	        TabSpec groupSpec = tabHost.newTabSpec("Groups");
+	        // setting Title and Icon for the Tab
+	        //photospec.setIndicator("Photos", getResources().getDrawable(R.drawable.icon_photos_tab));
+	        groupSpec.setIndicator("Groups");
+	        Intent groupIntent = new Intent(this, GroupsActivity.class);
+	        groupSpec.setContent(groupIntent);
+	        
+	        // Adding all TabSpec to TabHost
+	        tabHost.addTab(welcomeSpec); 
+	        tabHost.addTab(eventSpec); 
+	        tabHost.addTab(groupSpec);
+	        
+	      //set Windows tab as default (zero based)
+			tabHost.setCurrentTab(2);
+	}
+}
